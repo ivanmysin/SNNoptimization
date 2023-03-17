50f7d0d02c7374a4a00cf9ba6463c98660471141
Ivan Mysin <imysin@mail.ru>
2023-03-17 13:45:45 +0300

    Plastic synapses in progress

--- brian_models/brian_plastic_synapse.py
+++ brian_models/brian_plastic_synapse.py
@@ -2,24 +2,40 @@ import numpy as np
 import net_lib
 from brian2 import *
 import matplotlib.pyplot as plt
+from plastic_synapse_ode import Synapse
+
+defaultclock.dt = 0.05*ms
 
 generator_params = {
     "name" : "ec3",
-    "R": 0.2,
+    "R": 0.6,
     "freq": 6.0,
-    "mean_spike_rate": 5.5, # 5,
+    "mean_spike_rate": 15.5, # 5,
     "phase": -1.57,
 }
+
+ec32ngf = {
+    "W": 1.0,
+    "pre_name": "ca3pyr",
+    "post_name": "aac",
+    "tau_f": 31.42111393,
+    "tau_r": 388.4010873,
+    "tau_d": 5.516572108,
+    "Uinc": 0.203836307,
+    "gbarS": 0.991873144,
+    "Erev": 0.0,
+}
+
 duration = 500 * ms
-N = 100
+N = 1
 
 poisson_input = PoissonGroup(N, rates=net_lib.get_generator_rates(generator_params))
 poisson_SkM = SpikeMonitor(poisson_input)
 
-defaultclock.dt = 0.1*ms
+
 Cm = 1*uF # /cm**2
 gL = 0.18 * mS
-EL = -60*mV
+EL = -70*mV
 ENa = 55*mV
 EK = -90*mV
 gNa = 50 * mS # 50 * msiemens # 150*msiemens
@@ -28,6 +44,10 @@ gK = 23 * mS  # 23*msiemens
 sigma = 0.3*mV
 
 Esyn = 0*mV
+
+tau_f = ec32ngf["tau_f"]*ms
+tau_r = ec32ngf["tau_r"]*ms
+tau_d = ec32ngf["tau_d"]*ms
 
 # Interneuron Model without A-current
 full_eqs = '''
@@ -41,30 +61,65 @@ beta_m = 4*exp(-(V+65*mV)/(18*mV))/ms : Hz
 dh/dt = (alpha_h*(1-h)-beta_h*h) : 1
 alpha_h = 0.07*exp(-(V+65*mV)/(20*mV))/ms : Hz
 beta_h = 1./(exp(-0.1/mV*(V+35*mV))+1)/ms : Hz
-#dn/dt = (alpha_n*(1-n)-beta_n*n) : 1
-#alpha_n = 0.1 / exprel(-(V+55*mV)/(10*mV))/ms : Hz
-#beta_n = 0.125*exp(-(V+65*mV)/(80*mV))/ms : Hz
+
+# dn/dt = (alpha_n*(1-n)-beta_n*n) : 1
+# alpha_n = 0.1 / exprel(-(V+55*mV)/(10*mV))/ms : Hz
+# beta_n = 0.125*exp(-(V+65*mV)/(80*mV))/ms : Hz
+
 dn/dt = (n_inf - n) / tau_n : 1
 tau_n = 0.5*ms + 2.0*ms/(1 + exp(0.045/mV * (V - 50*mV))) : second
 n_inf = 1.0 / (1.0 + exp(-0.045 * (V/mV + 10))) : 1
 
 Isyn = gsyn*(Esyn - V)    : ampere
-gsyn = A_S * uS : simens
+gsyn = A_S * 1 * mS : siemens
 
-
+dU_S/dt = -U_S/tau_f : 1
+dR_S/dt = (1 - R_S - A_S) / tau_r : 1
+dA_S/dt = -A_S/tau_d : 1
 '''
 
-full_neuron = NeuronGroup(1, full_eqs, method='exponential_euler', namespace={"Iext" : 0.5*uA}, threshold='V>-10*mV')
+full_neuron = NeuronGroup(1, full_eqs, method='exponential_euler', namespace={"Iext" : 0.0*uA}, threshold='V>-10*mV')
 full_neuron.V = -60*mV
 full_neuron.n = 0.09
 full_neuron.h = 1.0
+full_neuron.R_S = 1.0
 
+neuron_mon = StateMonitor(full_neuron, variables=["A_S", "U_S", "R_S", "V"], record=0)
+
+on_pre = """
+U_S_post += 0.218766637*(1 - U_S)
+R_S_post -= U_S_post*R_S_post 
+A_S_post += U_S_post*R_S_post 
+"""
+synapse = Synapses(poisson_input, full_neuron, on_pre=on_pre)
+synapse.connect()
 
 run(duration)
 
-fig, axes = plt.subplots()
 
-axes.scatter(poisson_SkM.t/ms, poisson_SkM.i)
+################################
+synapse = Synapse(ec32ngf)
+y0 = np.asarray([1.0, 0.0, 0.0])
+t = np.arange(0, 500, 0.05)
+SRpre = np.zeros_like(t)
+
+SRpre[ (poisson_SkM.t/ms / t[1]).astype(np.int32) ] = 1
+solution = synapse.integrate(t, y0, SRpre)
+
+
+fig, axes = plt.subplots(nrows=4)
+
+#axes.scatter(poisson_SkM.t/ms, poisson_SkM.i)
+axes[0].plot(neuron_mon.t/ms, neuron_mon.V[0]/mV)
+
+axes[1].plot(neuron_mon.t/ms, neuron_mon.A_S[0])
+axes[1].plot(t, solution[:, 1])
+
+axes[2].plot(neuron_mon.t/ms, neuron_mon.R_S[0])
+axes[2].plot(t, solution[:, 0])
+
+axes[3].plot(neuron_mon.t/ms, neuron_mon.U_S[0])
+axes[3].plot(t, solution[:, 2])
 
 plt.show()
 

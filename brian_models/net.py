5269ccd38937f89a2c8e0131fcfdd947ebbefaa9
Ivan Mysin <imysin@mail.ru>
2023-03-17 15:13:14 +0300

    Monte-Carlo simulations in progress

--- brian_models/net.py
+++ brian_models/net.py
@@ -1,34 +1,34 @@
 import numpy as np
 import sys
+
 sys.path.extend(['/home/ivan/PycharmProjects/SNNoptimization'])
 from brian2 import *
-defaultclock.dt = 0.01*ms
+
+defaultclock.dt = 0.05 * ms
 import pickle
-#from code_generated_params import params_net
+# from code_generated_params import params_net
 import net_lib
 import h5py
 
-
-METHOD = 'heun'  #'exponential_euler'
-NNP = 200 # 2000
-PCONN = 0.1
+METHOD = 'heun'  # 'exponential_euler'
+NNP = 200  # 2000
+PCONN = 0.5
 
 
 def get_int_A_group(params_groups):
     N = NNP
     params = {
         "Iext": params_groups["Iext"] * uA,
-        "Cm" : params_groups["C"] * uF,  # /cm**2
-        "gL" : params_groups["gl"] * mS,
-        "EL" : params_groups["El"] * mV,
-        "ENa" : 55 * mV,
-        "EK" : params_groups["channels_params"][0]["Erev"] * mV,
-        "gNa" : 50 * mS,
-        "gK" : params_groups["channels_params"][0]["gmax"] * mS,
-        "gKA" : params_groups["channels_params"][1]["gmax"] * mS,
-        "sigma" : 0.3 * mV,
+        "Cm": params_groups["C"] * uF,  # /cm**2
+        "gL": params_groups["gl"] * mS,
+        "EL": params_groups["El"] * mV,
+        "ENa": 55 * mV,
+        "EK": params_groups["channels_params"][0]["Erev"] * mV,
+        "gNa": 50 * mS,
+        "gK": params_groups["channels_params"][0]["gmax"] * mS,
+        "gKA": params_groups["channels_params"][1]["gmax"] * mS,
+        "sigma": 0.3 * mV,
     }
-
 
     # Interneuron Model with A-current
     eqs = '''
@@ -64,8 +64,7 @@ def get_int_A_group(params_groups):
     {isyn_str}
     '''
     Isyn_str = net_lib.get_str4Isyn(params_groups, params_net["params_synapses"], NNP, PCONN)
-    eqs = eqs.format(isyn_str = Isyn_str)
-
+    eqs = eqs.format(isyn_str=Isyn_str)
 
     neuron = NeuronGroup(N, eqs, method=METHOD, namespace=params, name=params_groups["name"], threshold='V > -20*mV')
     neuron.V = -90 * mV
@@ -77,16 +76,15 @@ def get_ngf_group(params_groups):
     N = NNP
     params = {
         "Iext": params_groups["Iext"] * uA,
-        "Cm" : params_groups["C"] * uF,  # /cm**2
-        "gL" : params_groups["gl"] * mS,
-        "EL" : params_groups["El"] * mV,
-        "ENa" : 55 * mV,
-        "EK" : params_groups["channels_params"][0]["Erev"] * mV,
-        "gNa" : 50 * mS,
-        "gK" : params_groups["channels_params"][0]["gmax"] * mS,
-        "sigma" : 0.3 * mV,
+        "Cm": params_groups["C"] * uF,  # /cm**2
+        "gL": params_groups["gl"] * mS,
+        "EL": params_groups["El"] * mV,
+        "ENa": 55 * mV,
+        "EK": params_groups["channels_params"][0]["Erev"] * mV,
+        "gNa": 50 * mS,
+        "gK": params_groups["channels_params"][0]["gmax"] * mS,
+        "sigma": 0.3 * mV,
     }
-
 
     # Interneuron Model
     eqs = '''
@@ -94,7 +92,7 @@ def get_ngf_group(params_groups):
     IL = gL*(EL - V)           : ampere
     INa = gNa*m**3*h*(ENa - V) : ampere
     IKdr = gK*n**4*(EK - V) : ampere
-    
+
     m = alpha_m/(alpha_m+beta_m) : 1
     alpha_m = 1.0 / exprel(-(V+40*mV)/(10*mV))/ms : Hz
     beta_m = 4*exp(-(V+65*mV)/(18*mV))/ms : Hz
@@ -104,7 +102,7 @@ def get_ngf_group(params_groups):
     #dn/dt = (alpha_n*(1-n)-beta_n*n) : 1
     #alpha_n = 0.1 / exprel(-(V+55*mV)/(10*mV))/ms : Hz
     #beta_n = 0.125*exp(-(V+65*mV)/(80*mV))/ms : Hz
-    
+
     dn/dt = (n_inf - n) / tau_n : 1
     tau_n = 0.5*ms + 2.0*ms/(1 + exp(0.045/mV * (V - 50*mV))) : second
     n_inf = 1.0 / (1.0 + exp(-0.045 * (V/mV + 10))) : 1
@@ -112,13 +110,13 @@ def get_ngf_group(params_groups):
     {isyn_str}
     '''
     Isyn_str = net_lib.get_str4Isyn(params_groups, params_net["params_synapses"], NNP, PCONN)
-    eqs = eqs.format(isyn_str = Isyn_str)
-
+    eqs = eqs.format(isyn_str=Isyn_str)
 
     neuron = NeuronGroup(N, eqs, method=METHOD, namespace=params, name=params_groups["name"], threshold='V > -20*mV')
     neuron.V = -90 * mV
 
     return neuron
+
 
 def get_olm_group(params_groups):
     N = NNP
@@ -145,27 +143,27 @@ def get_olm_group(params_groups):
     IKdr = gK*n**4*(EK - V) : ampere
     IKA = gKA * a*b * (EK - V) :  ampere
     IH = gH * rH * (EH - V) :  ampere
-    
+
     m = alpha_m / (alpha_m + beta_m) : 1
     alpha_m = 1.0 / exprel(-(V+38*mV)/(10*mV))/ms : Hz
     beta_m = 4*exp(-(V+65*mV)/(18*mV))/ms : Hz
-    
+
     dh/dt = (alpha_h*(1-h)-beta_h*h) : 1
     alpha_h = 0.07*exp(-(V+63*mV)/(20*mV))/ms : Hz
     beta_h = 1./(exp(-0.1/mV*(V+33*mV))+1)/ms : Hz
-    
+
     dn/dt = (alpha_n*(1-n)-beta_n*n) : 1
     alpha_n = 0.018/mV * (V - 25*mV) / (1 - exp( (V - 25*mV)/(-25*mV)) )/ms : Hz
     beta_n = 0.0036*((V-35*mV)/mV) / (exp( (V-35*mV)/(12*mV) ) - 1) /ms : Hz
-    
+
     da/dt = (a_inf - a) / tau_a : 1
     a_inf = 1 / (1 + exp( (V + 14*mV)/(-16*mV) ) ) : 1
     tau_a = 5*ms : second
-    
+
     db/dt = (b_inf - b) / tau_b : 1
     b_inf = 1 / (1 + exp( (V + 71*mV)/(7.3*mV) ) ) : 1
     tau_b = 1 / (0.000009/(exp((V - 26*mV)/(18.5*mV)) + 0.014/(0.2 + exp((V+70*mV)/(-11*mV)))))*ms : second
-   
+
     drH/dt = (rH_inf - rH) / tau_rH : 1
     rH_inf =  1 / (1 + exp( (V + 84*mV) / (10.2*mV) ) ) : 1
     tau_rH = 1 / (exp(-14.59 - 0.086*V/mV) + exp(-1.87 + 0.0701*V/mV) ) * ms : second
@@ -181,6 +179,8 @@ def get_olm_group(params_groups):
     neuron.h = 1.0
 
     return neuron
+
+
 def get_connection_object(pre_pop, post_pop, syn_params):
     # U_0, tau_r, tau_f, w, gbarS
     synapses_eqs_template = '''
@@ -206,12 +206,10 @@ def get_connection_object(pre_pop, post_pop, syn_params):
     # print(synapses_action)
     # print("#############################")
 
-
-    synobj = Synapses(pre_pop, post_pop,  model=synapses_eqs,
-                       on_pre=synapses_action, namespace={"U_0": syn_params["Uinc"], "W_inp":1*nS})
-    synobj.connect(p=PCONN) #set w !!!!
+    synobj = Synapses(pre_pop, post_pop, model=synapses_eqs,
+                      on_pre=synapses_action, namespace={"U_0": syn_params["Uinc"], "W_inp": 1 * nS})
+    synobj.connect(p=PCONN)  # set w !!!!
     synobj.x_S = 1
-
 
     return synobj
 
@@ -233,14 +231,14 @@ def filtrate_params_net(params_net):
         if (synapse["pre_name"] in generators_names) and (synapse["post_name"] in neurons_names):
             params_net_filtared["params_synapses"].append(synapse)
 
-
-
     return params_net_filtared
+
+
 ###################################################################################################
 #
 with open('/home/ivan/Data/Opt_res/params_net.pickle', 'rb') as file:
     params_net = pickle.load(file)
-#params_net = filtrate_params_net(params_net)
+# params_net = filtrate_params_net(params_net)
 
 
 Net = Network()
@@ -259,14 +257,13 @@ for neural_population_idx, params_neurons in enumerate(params_net["params_neuron
     if params_neurons["name"] in ["pvbas", "cckbas", "aac", "ivy", "bis"]:
         neuron_group = get_int_A_group(params_neurons)
     if params_neurons["name"] in ["ngf", ]:
-         neuron_group = get_ngf_group(params_neurons)
+        neuron_group = get_ngf_group(params_neurons)
     if params_neurons["name"] in ["olm", ]:
-         neuron_group = get_olm_group(params_neurons)
+        neuron_group = get_olm_group(params_neurons)
 
     SpkMon = SpikeMonitor(neuron_group)
     SpkMons.append(SpkMon)
     Net.add(neuron_group, SpkMon)
-
 
 for pre_pop in Net.sorted_objects:
     for post_pop in Net.sorted_objects:
@@ -276,21 +273,20 @@ for pre_pop in Net.sorted_objects:
                 synapses = get_connection_object(pre_pop, post_pop, syn_params)
                 Net.add(synapses)
 
-Net.run(1800*ms, report='text')
+Net.run(1800 * ms, report='text')
 
 resfile = h5py.File('/home/ivan/Data/interneurons_theta/Monte_Carlo.hdf5', "w")
-#fig, axes = plt.subplots(nrows=len(SpkMons), sharex=True)
+# fig, axes = plt.subplots(nrows=len(SpkMons), sharex=True)
 for pops_idx, SpkMon in enumerate(SpkMons):
-    #print( np.asarray(SpkMon.t).size / 0.2)
-    #print(SpkMon.source.name)
+    # print( np.asarray(SpkMon.t).size / 0.2)
+    # print(SpkMon.source.name)
     # axes[pops_idx].scatter(SpkMon.t/ms, SpkMon.i)
     # axes[pops_idx].set_title(SpkMon.source.name)
-    resfile.create_dataset(SpkMon.source.name+"_times", data=SpkMon.t/ms)
-    resfile.create_dataset(SpkMon.source.name+"_indexes", data=SpkMon.i)
+    resfile.create_dataset(SpkMon.source.name + "_times", data=SpkMon.t / ms)
+    resfile.create_dataset(SpkMon.source.name + "_indexes", data=SpkMon.i)
 
 resfile.close()
-#axes.plot(M_full_V.t/ms, M_full_V[0].V/mV)
-#plt.show()
-
+# axes.plot(M_full_V.t/ms, M_full_V[0].V/mV)
+# plt.show()
 
 

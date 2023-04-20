0631724d31dbe5e20bda99b2b93c3aefea574e6d
Ivan Mysin <imysin@mail.ru>
2023-04-20 19:22:33 +0300

    LFP is start to process.

--- cbrd_tests/test_net_wth_two_compartments.py
+++ cbrd_tests/test_net_wth_two_compartments.py
@@ -14,7 +14,8 @@ stength = [3.0, 3.0]
 net.set_compartmets(1, stength)
 
 y0 = net.get_y0()
-t = tf.range(0, 1000, 0.1, dtype=tf.float64)
+dt = 0.1
+t = tf.range(0, 1000, dt, dtype=tf.float64)
 
 solution = net.run_simulation(t, y0)
 
@@ -22,16 +23,32 @@ h5pyfile = h5py.File('/home/ivan/Data/test/som.hdf5', mode='w')
 h5pyfile.create_dataset('solution', data=solution)
 h5pyfile.close()
 
-fig, axes = plt.subplots(nrows=len(params_net["params_neurons"]))
+rho = solution[:, 0:400]
+Vs = solution[:, 400:800]
+Vd = solution[:, 800:1200]
+
+lfp_1 = tf.math.reduce_sum( rho * (Vd - Vs), axis=1)
+
+lfp_2 = tf.math.reduce_sum( rho[1:, :] * ( -(Vs[:-1, :] - Vs[1:, :]) / dt + stength[0] * (Vd[1:, :] - Vs[1:, :]) ), axis=1)
+#lfp_2 = tf.math.reduce_sum( rho * (Vd - Vs), axis=1)
+
+fig, axes = plt.subplots(nrows=2)
+axes[0].plot(t, lfp_1)
+axes[1].plot(t[1:], lfp_2)
 
 
-for idx in range(len(params_net["params_neurons"])):
-    try:
-        firings = solution[:, net.neurons[idx].ro_start_idx]
-    except AttributeError:
-        continue
 
-    axes[idx].plot(t, firings)
-    axes[idx].set_title(net.neurons[idx].population_name)
 
+# fig, axes = plt.subplots(nrows=len(params_net["params_neurons"]))
+#
+#
+# for idx in range(len(params_net["params_neurons"])):
+#     try:
+#         firings = solution[:, net.neurons[idx].ro_start_idx]
+#     except AttributeError:
+#         continue
+#
+#     axes[idx].plot(t, firings)
+#     axes[idx].set_title(net.neurons[idx].population_name)
+#
 plt.show()
